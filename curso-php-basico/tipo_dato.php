0919682d497799dce573b62bbe86d0b197514e61
kimha1030 <kimha1030@gmail.com>
2023-03-13 00:05:08 -0500

    Clase tipo de datos

--- curso-php-basico/tipo_dato.php
+++ curso-php-basico/tipo_dato.php
@@ -29,4 +29,58 @@ Caracteristicas
   Ejm: Si suma un string con un numero, tomara ambos como nums (casteara el str a int)
 */
 
+$num_str = "23";
+$suma = 23 + $num_str;
+//echo $suma;
+
+$num = 10;
+$suma_num = 0.5 + $num;
+//var_dump($suma_num);
+
+$num_dos = "10 numeros en un string";
+$suma_dos = $num_dos + 30;
+//var_dump($suma_dos);
+
+$numero_str = "5";
+var_dump($numero_str);
+
+$numero_cast = (int) $numero_str;
+var_dump($numero_cast);
+
+$numero_float = 5.89;
+$float_cast = (int) $numero_float;
+var_dump($float_cast);
+
+$bandera = 0;
+$bandera = (bool) $bandera;
+var_dump($bandera);
+
+/*
+RETO 1 
+Que tipo de dato son las siguientes variables?
+*/
+
+$nombre = "Carlos";  //str
+var_dump($nombre);
+$apellido = "Gomez";  //str
+var_dump($apellido);
+$edad = 23;  //int
+var_dump($edad);
+$aprobado = true;  //bool
+var_dump($aprobado);
+$promedio = (8 + 9.5 + 9 + 19 + 8) / 5; //float
+var_dump($promedio);
+$nombre_completo = "$nombre $apellido";  //str
+var_dump($nombre_completo);
+$presenta_examen = (bool) 1; //true
+var_dump($presenta_examen);
+$numero_preguntas = 5 + "10"; //Lo castea a int
+var_dump($numero_preguntas);
+$numero_respuestas = "10" + 0.5; //Lo castea a float
+var_dump($numero_respuestas);
+$prom_maximo = $numero_respuestas / 1; //float
+var_dump($prom_maximo);
+$michis = 3 + "5 michis"; //int - Solo lo convierte a int cuando el string comienza con el num
+var_dump($michis);
+
 
